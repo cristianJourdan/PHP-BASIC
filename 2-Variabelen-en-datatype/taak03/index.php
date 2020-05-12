ffed7aa26bf0205dce3b1ddb1a5d94be4cc7bb07
cristianJourdan <2106549@talnet.nl>
2020-05-12 12:01:40 +0200

    2 taak 2 en 3

---
+++ 2-Variabelen-en-datatype/taak03/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "test";
+    $var_leeftijd_ = 16;
+    $var_bananen_ = 5,49;
+    echo $var_sleeftijd + $var_bananen_;
+     ?>
+</body>
+</html>
