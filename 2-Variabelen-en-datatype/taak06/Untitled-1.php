f8fd9c585da7825be4c9d9b2722da79436854586
cristianJourdan <2106549@talnet.nl>
2020-05-14 11:44:38 +0200

    taak 5 t/m 7 8 lukt niet

---
+++ 2-Variabelen-en-datatype/taak06/Untitled-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "test";
+    $string = "string";
+    $integer = 9;
+    $float = 6.9;
+    $boolean = true;
+    $null = null;
+    echo gettype($string);
+    echo gettype($integer);
+    echo gettype($float);
+    echo gettype($boolean);
+    echo gettype($null);
+     ?>
+</body>
+</html>
---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "test";
+    $voornaam1_ = "peter";
+    $voornaam2_ = "peter2";
+    $voornaam3_ = "peter3";
+
+     ?>
+</body>
+</html>
