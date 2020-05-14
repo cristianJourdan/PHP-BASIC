732d6ea66d54f0874d350303a22c81d81f806433
cristianJourdan <2106549@talnet.nl>
2020-05-14 12:24:13 +0200

    taak 1 arrays

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "test";
+    $_myMovies_ = ["spiderman","frozen","batman"]
+    echo "<ul>";
+    echo "<li>";
+    echo $_myMovies_[0]
+    echo "</li>";
+    echo "<li>";
+    echo $_myMovies_[1];
+    echo "</li>";
+    echo "<li>";
+    echo $_myMovies_[2];
+    echo "</li>";
+    echo "</ul>";
+     ?>
+</body>
+</html>
