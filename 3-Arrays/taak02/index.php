68ec2ca8d8a31f885658dd1e76d3575e0965d3e2
cristianJourdan <2106549@talnet.nl>
2020-05-14 12:28:22 +0200

    arrays taak 2

---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "test";
+    $_myDogNames = ["peter","bas","johan"]
+    $_myDogNames[1] = "tijger";
+
+    echo .$_myDogNames[0]. ",".$_myDogNames[1]. ",".$_myDogNames[2].;
+
+     ?>
+</body>
+</html>
