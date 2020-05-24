db7a1719922d07b1dccd4fc338fba9f805ecbf7f
cristianJourdan <2106549@talnet.nl>
2020-05-24 19:43:40 +0200

    h4 arithmetics

---
+++ 4.5-Arithmetics/taak01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "test";
+    $getal_ = 5;
+    $getal1_ = 10;
+    echo  $getal_ + $getal1_;
+    echo  $getal_ - $getal1_;
+    echo  $getal_ * $getal1_;
+    echo  $getal_ / $getal1_;
+
+     ?>
+</body>
+</html>
---
+++ 4.5-Arithmetics/taak02/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "test";
+    $getal1_ = 10;
+    echo  $getal_ += 10"<br>";
+    echo  $getal_ -= 10"<br>";
+    echo  $getal_ *= 10"<br>";
+    echo  $getal_ /= 10"<br>";
+
+     ?>
+</body>
+</html>
