d0748724535bf37698302c6b9f7ed38b18a5be61
cristianJourdan <2106549@talnet.nl>
2020-05-24 16:34:24 +0200

    h7

---
+++ 7-Include-Require/taak01/menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <div class="menu">
+        <?php include 'index.php';?>
+    </div>
+
+</head>
+<body>
+<?php
+$var1 ="menu-item1";
+$var2 ="menu-item2";
+$var3 ="menu-item3";
+
+echo "<ul><li>"$var1"</li><li>"$var2"</li><li>"$var3"</li></ul>"
+
+?>
+
+
+</body>
+</html>
